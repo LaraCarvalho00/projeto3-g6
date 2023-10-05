3776445d73b30ef53ebf5b135e447d0aaf579916
Otavio Salomao <osfsilva@sga.pucminas.br>
2023-10-04 21:04:49 -0300

    Add files via upload

---
+++ codigo/Vaga.java
@@ -0,0 +1,24 @@
+
+
+public class Vaga {
+
+	private String id;
+	private boolean disponivel;
+
+	public Vaga(int fila, int numero) {
+		
+	}
+
+	public boolean estacionar() {
+		
+	}
+
+	public boolean sair() {
+		
+	}
+
+	public boolean disponivel() {
+		
+	}
+
+}
---
+++ codigo/Veiculo.java
@@ -0,0 +1,32 @@
+
+
+public class Veiculo {
+
+	private String placa;
+	private UsoDeVaga[] usos;
+
+	public Veiculo(String placa) {
+		
+	}
+
+	public void estacionar(Vaga vaga) {
+		
+	}
+
+	public double sair() {
+		
+	}
+
+	public double totalArrecadado() {
+		
+	}
+
+	public double arrecadadoNoMes(int mes) {
+		
+	}
+
+	public int totalDeUsos() {
+		
+	}
+
+}
