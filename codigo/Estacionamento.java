ed74ddccafe2c70fa3dbe488706ba6982bcacf86
Larissa <larissafelipereis7@gmail.com>
2023-10-04 22:24:35 -0300

    implementação classe estacionamento

--- codigo/Estacionamento.java
+++ codigo/Estacionamento.java
@@ -1,22 +1,39 @@
-
-
 public class Estacionamento {
 
 	private String nome;
-	private Cliente[] id;
+	private Cliente[] clientes;
 	private Vaga[] vagas;
 	private int quantFileiras;
 	private int vagasPorFileira;
 
 	public Estacionamento(String nome, int fileiras, int vagasPorFila) {
-		
+		this.nome = nome;
+        this.quantFileiras = fileiras;
+        this.vagasPorFileira = vagasPorFila;
+        this.clientes = new Cliente[100]; 
+        this.vagas = new Vaga[fileiras][vagasPorFila];
+        this.totalArrecadado = 0.0;
+        gerarVagas();
 	}
 
 	public void addVeiculo(Veiculo veiculo, String idCli) {
-		
+		Cliente buscando = new Cliente(idCli, idCli);
+		Cliente atual = null;
+		for (int i = 0; i < clientes.length; i++) {
+			if(clientes[i].equals(buscando)){
+				atual = clientes[i];
+				break;
+			}
+		}
+		if(atual!=null){
+			atual.addVeiculo(veiculo);
+
+		}
 	}
 
+
 	public void addCliente(Cliente cliente) {
+		
 		
 	}
 
