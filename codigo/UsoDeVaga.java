9a874cc19cd03c2c40752a2879bc9f813f807707
Salomao0tavio <otavio.ferreira.mds@gmail.com>
2023-10-04 22:27:59 -0300

    Correcao classe

--- codigo/UsoDeVaga.java
+++ codigo/UsoDeVaga.java
@@ -5,7 +5,7 @@ public class UsoDeVaga {
     private static final double FRACAO_USO = 0.25;
     private static final double VALOR_FRACAO = 4.0;
     private static final double VALOR_MAXIMO = 50.0;
-    //private Vaga vaga;
+    private Vaga vaga;
     private LocalDateTime entrada;
     private LocalDateTime saida;
     private double valorPago;
