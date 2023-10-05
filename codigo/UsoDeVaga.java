56617936ffe1eb85b80a71c05d85f95448c3ff52
Salomao0tavio <otavio.ferreira.mds@gmail.com>
2023-10-04 22:27:25 -0300

    UsoDeVaga implementando metodos

--- codigo/UsoDeVaga.java
+++ codigo/UsoDeVaga.java
@@ -1,29 +1,45 @@
-
+import java.time.LocalDateTime;
 
 public class UsoDeVaga {
 
-	private static final double FRACAO_USO = 0.25;
-	private static final double VALOR_FRACAO = 4.0;
-	private static final double VALOR_MAXIMO = 50.0;
-	private Vaga vaga;
-	private LocalDateTime entrada;
-	private LocalDateTime saida;
-	private double valorPago;
+    private static final double FRACAO_USO = 0.25;
+    private static final double VALOR_FRACAO = 4.0;
+    private static final double VALOR_MAXIMO = 50.0;
+    //private Vaga vaga;
+    private LocalDateTime entrada;
+    private LocalDateTime saida;
+    private double valorPago;
 
-	public UsoDeVaga(Vaga vaga) {
-		
-	}
+    public UsoDeVaga(Vaga vaga) {
+        this.vaga = vaga;
+        this.entrada = LocalDateTime.now();
+    }
 
-	public double sair() {
-		
-	}
+    public double sair() {
+        this.saida = LocalDateTime.now();
+        long minutosEstacionado = calcularMinutosEstacionado();
+        double valorPago = calcularValorPago(minutosEstacionado);
+       
+        return valorPago;
+    }
 
-	public boolean ehDoMes(int mes){
-		
-	}
-	
-	public double valorPago() {
-		
-	}
+    private long calcularMinutosEstacionado() {
+        return entrada.until(saida, java.time.temporal.ChronoUnit.MINUTES);
+    }
 
+    private double calcularValorPago(long minutosEstacionado) {
+        double valor = minutosEstacionado * FRACAO_USO * VALOR_FRACAO;
+		if(valor > VALOR_MAXIMO){
+			valor = 50;
+		}
+        return valor;
+    }
+
+    public boolean ehDoMes(int mes) {
+        return entrada.getMonthValue() == mes;
+    }
+
+    public double valorPago() {
+        return valorPago;
+    }
 }
