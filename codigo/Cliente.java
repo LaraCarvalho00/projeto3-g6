4e20b44cc5b85d84acbb5c05c70362095ac32bf6
Larissa <larissafelipereis7@gmail.com>
2023-10-19 19:54:27 -0300

    Voltando commit

--- codigo/Cliente.java
+++ codigo/Cliente.java
@@ -1,5 +1,4 @@
 
-
 public class Cliente {
 
 	private String nome;
@@ -7,31 +6,55 @@ public class Cliente {
 	private Veiculo[] veiculos;
 
 	public Cliente(String nome, String id) {
-		
+		this.nome = nome;
+		this.id = id;
+		this.Veiculo = new Veiculo[0];
+
 	}
 
 	public void addVeiculo(Veiculo veiculo) {
-		
+		int tamanhoAtual = veiculos.length;
+		Veiculo[] novoArray = new Veiculo[tamanhoAtual + 1];
+		for (int i = 0; i < tamanhoAtual; i++) {
+			novoArray[i] = veiculos[i];
+
+		}
+		novoArray[tamanhoAtual] = veiculo;
+		veiculos = novoArray;
 	}
 
 	public Veiculo possuiVeiculo(String placa) {
-		
+		for (Veiculo veiculo : veiculos) {
+			if (veiculo.getPlaca().equals(placa)) {
+				return veiculo;
+			}
+		}
+		return null;
 	}
 
 	public int totalDeUsos() {
-		
+		return veiculos.length;
+
 	}
 
 	public double arrecadadoPorVeiculo(String placa) {
-		
+		double arrecadacaoTotal = 0.0;
+		for (Veiculo veiculo : veiculos) {
+			if (veiculo.getPlaca().equals(placa)) {
+				arrecadacaoTotal += veiculo.getArrecadacao();
+				return arrecadacaoTotal;
+			}			
+		}
+		return 0.0;
 	}
 
-	public double arrecadadoTotal() {
-		
-	}
 
 	public double arrecadadoNoMes(int mes) {
-		
+		double arrecadacaoNoMes = 0.0;
+		for (Veiculo veiculo : veiculos) {
+			arrecadacaoNoMes += veiculo.arrecadadoNoMes(mes);
+		}
+		return arrecadacaoNoMes;
 	}
 
 }
