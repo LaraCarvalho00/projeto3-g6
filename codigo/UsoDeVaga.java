4c06598318720cb481a9a8f53798817260a30b45
Davi Martins <125777453+DeviMartins1@users.noreply.github.com>
2023-12-06 17:11:15 -0300

    mudanças da iteraçao e DOC

--- codigo/UsoDeVaga.java
+++ codigo/UsoDeVaga.java
@@ -1,174 +1,94 @@
-import java.io.FileWriter;
-import java.io.IOException;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 
-// Classe que representa o uso de uma vaga de estacionamento
-public class UsoDeVaga{
-
-    // Constantes para cálculos de valor
-    private static final double FRACAO_USO = 0.25;
-    private static final double VALOR_FRACAO = 4.0;
-    private static final double VALOR_MAXIMO = 50.0;
+// Classe abstrata que representa o uso de uma vaga de estacionamento
+public abstract class UsoDeVaga {
 
     // Atributos da classe
-    private Vaga vaga;
-    private LocalDateTime entrada;
-    private LocalDateTime saida;
-    private double valorPago;
-    private int horaMinima;
+    Vaga vaga; // Vaga utilizada
+    LocalDateTime entrada; // Data e hora de entrada na vaga
+    LocalDateTime saida; // Data e hora de saída da vaga
+    double valorPago; // Valor pago pelo uso da vaga
+    Servico servico; // Serviço contratado durante o uso da vaga
 
-    // Getter e Setter para a hora mínima
-    public int getHoraMinima() {
-        return horaMinima;
-    }
-
-    public void setHoraMinima(int horaMinima) {
-        this.horaMinima = horaMinima;
-    }
-
-    // Construtor para inicializar um objeto UsoDeVaga com entrada
-    public UsoDeVaga(Vaga vaga, LocalDateTime entrada) {
+    public UsoDeVaga(Vaga vaga){
         this.vaga = vaga;
-        this.entrada = entrada;
+        entrada = LocalDateTime.now();
     }
-
-    // Construtor para inicializar um objeto UsoDeVaga com entrada, saída e valor pago
-    public UsoDeVaga(Vaga vaga, LocalDateTime entrada, LocalDateTime saida, double valorPago) {
+    public UsoDeVaga(Vaga vaga, Servico servico){
         this.vaga = vaga;
-        this.entrada = entrada;
-        this.saida = saida;
-        this.valorPago = valorPago;
+        contratarServico(servico);
+        entrada = LocalDateTime.now();
     }
-
-    // Getter para a vaga
-    public Vaga getVaga() {
-        return vaga;
-    }
-
-    // Método para escrever informações sobre o uso da vaga em um arquivo
-    public void escreverArquivo(String placa, String estacionamento) {
-        try {
-            FileWriter fileWriter = new FileWriter("usoDeVaga.txt", true);
-            fileWriter.write(placa + "," + this.entrada + "," + this.saida + "," + this.valorPago + ","
-                    + this.vaga.getId() + ";");
-            this.vaga.escreverArquivo(estacionamento);
-
-            fileWriter.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
+    
     // Método para adicionar serviços extras ao valor pago
-    public void adicionarServico(int tipo) {
-        if (tipo == 1) {
-            this.valorPago += 5;
-        } else if (tipo == 2) {
-            this.valorPago += 20;
-            this.horaMinima = 1;
-        } else if (tipo == 3) {
-            this.valorPago += 45;
-            this.horaMinima = 2;
+    public void contratarServico(Servico servico) {
+        this.servico = servico;
+    }
+
+    /**
+     * Verifica se o uso da vaga foi no mês fornecido.
+     *
+     * @param mes O mês a ser verificado (1 a 12 para janeiro a dezembro)
+     * @return true se o uso da vaga foi no mês fornecido, caso contrário false
+     */
+    public boolean ehDoMes(int mes) {
+        return entrada.getMonthValue() == mes;
+    }
+
+    /**
+     * Registra a saída da vaga e calcula o valor a ser pago, levando em consideração o tempo de permanência
+     * e os serviços contratados durante o uso da vaga.
+     *
+     * @return O valor total a ser pago pelo uso da vaga
+     */
+    public double sair() {
+        vaga.setDisponivel(true); // Define a vaga como disponível
+        this.saida = LocalDateTime.now(); // Registra a data e hora de saída
+        int tempoPermanenciaMinutos = (int) entrada.until(saida, ChronoUnit.MINUTES); // Calcula o tempo de permanência em minutos
+
+        // Verifica se serviços extras foram contratados
+        if (servico != null) {
+            // Verifica se o tempo de permanência é maior ou igual ao tempo mínimo do serviço contratado
+            if (tempoPermanenciaMinutos >= servico.getTempoMinimo()) {
+                // Retorna o valor total a ser pago considerando o valor da vaga e o preço do serviço
+                return valorPago() + servico.getPreco();
+            } else {
+                // Retorna apenas o valor da vaga, pois o tempo mínimo do serviço não foi atingido
+                return valorPago();
+            }
         }
+        // Retorna apenas o valor da vaga, pois nenhum serviço extra foi contratado
+        return valorPago();
     }
 
-    // Setter para a vaga
-    public void setVaga(Vaga vaga) {
-        this.vaga = vaga;
-    }
+    /**
+     * Método abstrato para calcular o valor a ser pago pelo uso da vaga.
+     *
+     * @return O valor a ser pago pelo uso da vaga
+     */
+    public abstract double valorPago();
 
-    // Getter para a entrada
+
     public LocalDateTime getEntrada() {
         return entrada;
     }
-
-    // Setter para a entrada
-    public void setEntrada(LocalDateTime entrada) {
-        this.entrada = entrada;
-    }
-
-    // Getter para a saída
     public LocalDateTime getSaida() {
         return saida;
     }
+    
+    /**
+     * Método para converter informações do uso de vaga em texto.
+     *
+     * @return Uma representação em texto das informações do uso de vaga
+     */
+    public String dataToText() {
+        StringBuilder data = new StringBuilder();
+        data.append("Data de entrada: ").append(entrada).append("\n");
+        data.append("Data de saída: ").append(saida).append("\n");
 
-    // Setter para a saída
-    public void setSaida(LocalDateTime saida) {
-        this.saida = saida;
+        return data.toString();
     }
 
-    // Getter para o valor pago
-    public double getvalorPago() {
-        return valorPago;
-    }
-
-    // Setter para o valor pago
-    public void setvalorPago(double valorPago) {
-        this.valorPago = valorPago;
-    }
-
-    // Método para calcular o valor a ser pago ao sair
-    public double sair(TipoCliente tipoCliente) {
-
-        if (entrada != null && saida != null) {
-            // Calcula a diferença entre a entrada e a saída com base no tipo de cliente
-            switch (tipoCliente) {
-                case HORISTA:
-                    return calcValue(entrada.toLocalTime(), saida.toLocalTime());
-                case TURNO_MANHA:
-                case TURNO_TARDE:
-                case TURNO_NOITE:
-                    return calcDiferenceValue(entrada.toLocalTime(), saida.toLocalTime(), tipoCliente.getHoraInicio(), tipoCliente.getHoraFim());
-                case MENSALISTA:
-                    return 0.0;
-                default:
-                    return 0.0;
-            }
-        }
-
-        return 0;
-    }
-
-    // Método para obter o valor pago
-    public double valorPago() {
-        return valorPago;
-    }
-
-    // Método para calcular o valor com base no tempo de uso
-    private double calcValue(LocalTime entrada, LocalTime saida) {
-        int minutosEstacionados = 0;
-        while (entrada.isBefore(saida)) {
-            minutosEstacionados++;
-            entrada = entrada.plusMinutes(1);
-        }
-        // Calcula o valor pela taxa de fração de uso
-        double valor = minutosEstacionados * FRACAO_USO * VALOR_FRACAO;
-
-        // Limita o valor ao valor máximo permitido
-        valorPago = Math.min(valor, VALOR_MAXIMO);
-        return valorPago;
-    }
-
-    // Método para calcular o valor com base na diferença de horários
-    private double calcDiferenceValue(LocalTime entrada, LocalTime saida, LocalTime horaInicio, LocalTime horaFim) {
-        LocalTime inicio = LocalTime.now();
-        LocalTime fim = LocalTime.now();
-        if (entrada.isAfter(horaFim)) {
-            inicio = entrada;
-            fim = saida;
-        } else if (saida.isAfter(horaFim)) {
-            inicio = horaFim;
-            fim = saida;
-        }
-        if (saida.isBefore(horaInicio)) {
-            inicio = entrada;
-            fim = saida;
-        } else if (entrada.isBefore(horaInicio)) {
-            inicio = entrada;
-            fim = horaInicio;
-        }
-        return calcValue(inicio, fim);
-    }
+    
 }
