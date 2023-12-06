f4fda7fd1e0cd22e241335378298834e2a7ef683
Davi Martins <125777453+DeviMartins1@users.noreply.github.com>
2023-12-06 17:11:51 -0300

    correçoes e DOC

--- codigo/Vaga.java
+++ codigo/Vaga.java
@@ -1,67 +1,100 @@
 import java.io.FileWriter;
 import java.io.IOException;
 
-// Classe que representa uma Vaga no estacionamento
+/**
+ * Classe que representa uma vaga no estacionamento.
+ */
 public class Vaga {
-	private String id;           // Identificação única da vaga
-	private boolean disponivel;  // Indica se a vaga está disponível
+    private String id;           // Identificação única da vaga
+    private boolean disponivel;  // Indica se a vaga está disponível
 
-	// Construtor para criar uma Vaga com uma identificação única
-	public Vaga(String id) {
+    /**
+     * Construtor para criar uma Vaga com uma identificação única.
+     *
+     * @param id Identificação única da vaga
+     */
+    public Vaga(String id) {
         this.id = id;
         this.disponivel = true;  // Inicialmente, a vaga está disponível
-	}
-
-	// Método para obter a identificação da vaga
-	public String getId() {
-		return this.id;
-	}
-
-	// Método para escrever informações da vaga em um arquivo
-	public void escreverArquivo(String estacionamento) {
-		 try {
-        FileWriter fileWriter = new FileWriter("vaga.txt", true);
-		fileWriter.write(estacionamento + "," + this.id + "," + this.disponivel + ";");      
-		System.out.println("Escrito uma vaga no arquivo.");       
-        fileWriter.close();
-      
-    } catch (IOException e) {
-        e.printStackTrace();
     }
-	}
 
-	// Método para configurar a identificação da vaga
-	public void setId(String id) {
-		this.id = id;
-	}
+    /**
+     * Obtém a identificação da vaga.
+     *
+     * @return Identificação da vaga
+     */
+    public String getId() {
+        return this.id;
+    }
 
-	// Método para verificar se a vaga está disponível
-	public boolean getDisponivel() {
-		return this.disponivel;
-	}
-	
-	// Método para configurar a disponibilidade da vaga
-	public void setDisponivel(boolean disponivel) {
-		this.disponivel = disponivel;
-	}
+    /**
+     * Obtém a disponibilidade da vaga.
+     *
+     * @return true se a vaga está disponível, false caso contrário
+     */
+    public boolean getDisponivel() {
+        return this.disponivel;
+    }
 
-	// Método para estacionar um veículo na vaga
-	public boolean estacionar() {
-		if (disponivel) {
-			this.disponivel = false;
-			return true;  // Retorna verdadeiro se o veículo foi estacionado com sucesso
-		} else {
-			return false;  // Retorna falso se a vaga já estiver ocupada
-		}
-	}
+    /**
+     * Configura a disponibilidade da vaga.
+     *
+     * @param disponivel true para marcar a vaga como disponível, false para ocupada
+     */
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
 
-	// Método para liberar a vaga, indicando que o veículo saiu
-	public boolean sair() {
-		if (!disponivel) {
-			this.disponivel = true;
-			return true;  // Retorna verdadeiro se a vaga foi liberada com sucesso
-		} else {
-			return false;  // Retorna falso se a vaga já estiver livre
-		}
-	}
+    /**
+     * Estaciona um veículo na vaga.
+     *
+     * @return true se o veículo foi estacionado com sucesso, false se a vaga já estiver ocupada
+     */
+    public boolean estacionar() {
+        if (disponivel) {
+            this.disponivel = false;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Libera a vaga, indicando que o veículo saiu.
+     *
+     * @return true se a vaga foi liberada com sucesso, false se a vaga já estiver livre
+     */
+    public boolean sair() {
+        if (!disponivel) {
+            this.disponivel = true;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Escreve informações da vaga em um arquivo.
+     *
+     * @param estacionamento Identificador do estacionamento
+     */
+    public void escreverArquivo(String estacionamento) {
+        try {
+            FileWriter fileWriter = new FileWriter("vaga.txt", true);
+            fileWriter.write(estacionamento + "," + this.id + "," + this.disponivel + ";");
+            System.out.println("Escrito uma vaga no arquivo.");
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Configura a identificação da vaga.
+     *
+     * @param id Identificação da vaga
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
 }
